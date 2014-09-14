8dd60d52f8d9cb789748e71965e9112ff7e9d29e
C.J. Bennett <cecil@uga.edu>
2014-09-13 22:35:39 -0400

    Alarm.java documentation cleanup

--- src/nachos/threads/Alarm.java
+++ src/nachos/threads/Alarm.java
@@ -19,7 +19,7 @@ public class Alarm {
     long wakeUpTime;
 
     /**
-     * Create a new waiting thread container.
+     * Create a new WaitingThread container.
      *
      * @param thread the thread that is waiting
      * @param wakeUpTime the system time at which the thread should wake
@@ -86,43 +86,19 @@ public class Alarm {
      * that should be run.
      */
     public void timerInterrupt() {
-    	/*
-    	 * So far this only yields the thread. 
-    	 */
-    	//TO-DO: Check for time delta to add sleeping thread to readyQueue. 
-    	//TO-DO: if(elapsedTime >= necessarySleepTime){ pop thread from sleepingList? }
-    	//TO-DO: This will need locks, due to multiple threads accessing
 
-/* 
-      // iterate through list of waiting threads and wake the appropriate ones
-      for(int i = 0; i < waitingThreads.size(); i++)
-      {
-	if(waitingThreads.get(i).getWakeUpTime() <= Machine.timer().getTime())
-	{
-	  // lock this area down to prevent race conditions in the ArrayList
-	  boolean intStatus = Machine.interrupt().disable();
-
-	  waitingThreads.remove(i).getThread().ready();
-
-	  Machine.interrupt().restore(intStatus);
-	}// if
-      }// for
-*/
-
+      // repeatedly check first element of minheap for all threads ready to run again
       for(WaitingThread firstElement = waitingThreads.peek();
 	  firstElement != null && firstElement.getWakeUpTime() <= Machine.timer().getTime();
 	  firstElement = waitingThreads.peek())
       {
-	// lock this area down to prevent race conditions in the PriorityQueue
+	// lock this area down to prevent race conditions in the heap
 	boolean intStatus = Machine.interrupt().disable();
 	
 	waitingThreads.poll().getThread().ready();
 	
 	Machine.interrupt().restore(intStatus);
       }// for
-
-    	
-      // KThread.currentThread().yield();
     }
 
     /**
@@ -141,50 +117,20 @@ public class Alarm {
      */
     public void waitUntil(long x) {
     	
-    	/* -Matty
-    	 * This needs to be refactored:
-    	 * Suspend caller thread until the current time + x has elapsed.
-    	 * Then shift to the ready queue of the timer interrupt handler(this must be altered as well) 
-    	 * 
-    	 */
-    	
-    	//Re-use timer call from old method? Not sure how else to get the system time
       long wakeTime = Machine.timer().getTime() + x;
 
       // make sure time hasn't already passed, if it has skip all this crap and return immediately
       if(wakeTime > Machine.timer().getTime())
       {
-	
-    	//TO-DO: "All thread queue methods must be invoked with <b>interrupts disabled</b>." <-- In ThreadQueue abstract class, 
-    	//so we should probably disable interrupts here in order to add the caller thread to the ThreadQueue readyQueue
-    	//I would think this to be the case if there is only one alarm, and any number of threads can access it at a time.
-    	
-	// prevent race conditions on the ArrayList
+	// prevent race conditions on the heap and put calling thread to sleep
 	boolean intStatus = Machine.interrupt().disable();
-    	
-    	//TO-DO: Central block for Alarm to suspend the thread and toss it onto a queue
-    	//Looks like ready queue should be accessed from timerInterrupt() 
-    	//Maybe add it to a "sleeping" list/queue so timerInterrupt() can check it as timer() calls it?
-    	//Queue would alright if they were chronologically sorted, but a list structure could be random and still function
-    	
+    	    	
 	waitingThreads.offer(new WaitingThread(KThread.currentThread(), wakeTime));
-	
-	//After it's on the queue safely, and its time delta is being tracked we can suspend the thread
+		
     	KThread.sleep();
 
     	//And remember to re-enable interrupts once this block is finished
     	Machine.interrupt().restore(intStatus);
       }// if
-      
-
-    	
-    	/*  OLD THINGS AND DRAGONS  */
-    		// for now, cheat just to get something working (busy waiting is bad)
-    		//long wakeTime = Machine.timer().getTime() + x;
-    		//while (wakeTime > Machine.timer().getTime())
-    		// KThread.yield();
-    	/*  OK, DRAGONS ARE GONE  */
     }
-
-  
 }
